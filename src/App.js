f13e7548fc428c1dad9a696b0aa8cf56b1fbd5ca
daigo1117 <haruyamadaigo1117@gmail.com>
2025-04-08 16:01:17 +0900

    エントリーページをApp.jsにルーティング追加

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SurveyForm from "./components/SurveyForm";
 import ResultPage from "./components/ResultPage";
 import EnterSerialPage from "./components/EnterSerialPage";
+import EntryOverviewPage from "./components/EntryOverviewPage";
 
 function App() {
   return (
     <Router>
       <Routes>
+      <Route path="/entry" element={<EntryOverviewPage />} /> {/* ← 追加！ */}
         <Route path="/serial" element={<EnterSerialPage />} />
         <Route path="/form" element={<SurveyForm />} />  {/* ← ここを修正！ */}
         <Route path="/result" element={<ResultPage />} />
